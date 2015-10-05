c89802a42d1f332ed3d5714ed883c8ca4151769a
Jaspreet Singh <js6570@yp.com>
2015-10-04 23:00:50 -0700

    Mostly done landing page, needs <a> tags for linking the intro text, portfolio aspect remains

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,32 @@
 module.exports = function (grunt) {
-grunt.initConfig({
-	responsive_images: {
-		dev: {
-			options: {},
-			sizes: [{
-				width: 320,
-				height: 240
-			}, {
-				name: 'large',
-				width: 640
-			}],
-			files: [{
-				expand: true,
-				src: ['styles/images/**/*.{jpg,gif,png}'],
-				cwd: 'resources/',
-				dest: 'dist/'
-			}]
+	grunt.initConfig({
+		responsive_images: {
+			dev: {
+				options: {
+					sizes: [{
+						name: 'small',
+						width: 240,
+						height: 240
+					}, {
+						name: 'large',
+						width: 640
+					}]
+				},
+				files: [{
+					expand: true,
+					src: ['styles/images/**/*.{jpg,gif,png}'],
+					cwd: 'resources/',
+					dest: 'dist/'
+				}]
+			}
+		},
+		clean: {
+			dist: ["dist/"]
 		}
-	}
-});
+	});
 
-grunt.loadNpmTasks('grunt-responsive-images');
-grunt.registerTask('default', ['responsive_images']);
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.registerTask('default', ['clean', 'responsive_images']);
 
 }
