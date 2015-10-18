be09dac0d8c70eb07393677cc8d207773b5d2e4b
Jaspreet Singh <js6570@yp.com>
2015-10-17 18:41:37 -0700

    Code rearrange

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		responsive_images: {
-			dev: {
-				options: {
-					sizes: [{
-						name: 'small',
-						width: 250
-					}, {
-						name: 'large',
-						width: 900
-					}, {
-						name: 'medium',
-						width: 365
-					}]
-				},
-				files: [{
-					expand: true,
-					src: ['styles/images/**/*.{jpg,gif,png}',
-						'!styles/images/**/*-large.{jpg,gif,png}',
-						'!styles/images/**/*-small.{jpg,gif,png}',
-						'!styles/images/**/*-medium.{jpg,gif,png}'],
-					cwd: 'resources/',
-					dest: 'dist/'
-				}]
-			}
-		},
-		clean: {
-			dist: ["dist/"]
-		}
-	});
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    sizes: [{
+                        name: 'small',
+                        width: 250
+                    }, {
+                        name: 'large',
+                        width: 900
+                    }, {
+                        name: 'medium',
+                        width: 365
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['styles/images/**/*.{jpg,gif,png}',
+                        '!styles/images/**/*-large.{jpg,gif,png}',
+                        '!styles/images/**/*-small.{jpg,gif,png}',
+                        '!styles/images/**/*-medium.{jpg,gif,png}'],
+                    cwd: 'resources/',
+                    dest: 'dist/'
+                }]
+            }
+        },
+        clean: {
+            dist: ["dist/"]
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-responsive-images');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.registerTask('default', ['clean', 'responsive_images']);
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['clean', 'responsive_images']);
 
 };
